5edd2b11b09b0fab0e77ed43ab3ac9ffbd3a7de7
Ephraim Solomon <efisolm22@gmail.com>
2024-10-01 16:01:57 +0300

    Integration issues fixed

--- src/app/modules/plan-tasks/entities/plan-task.entity.ts
+++ src/app/modules/plan-tasks/entities/plan-task.entity.ts
@@ -8,10 +8,17 @@ import {
   Tree,
   TreeChildren,
   TreeParent,
+  ValueTransformer,
 } from 'typeorm';
 import { KeyResult } from '../../key-results/entities/key-result.entity';
 import { Milestone } from '../../milestones/entities/milestone.entity';
 
+export const bigintTransformer: ValueTransformer = {
+  to: (value: number | null): string | null =>
+    value !== null ? value.toString() : null,
+  from: (value: string | null): number | null =>
+    value !== null ? parseInt(value, 10) : null,
+};
 @Entity()
 @Tree('closure-table')
 export class PlanTask extends BaseModel {
@@ -21,8 +28,8 @@ export class PlanTask extends BaseModel {
   @Column({ type: 'enum', enum: Priority, default: Priority.MEDIUM })
   priority: Priority;
 
-  @Column({ type: 'bigint', nullable: true })
-  targetValue: number;
+  @Column({ type: 'bigint', nullable: true, transformer: bigintTransformer })
+  targetValue: bigint;
 
   @Column({ type: 'int', nullable: true })
   weight: number;
