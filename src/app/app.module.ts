f153eb4a3fa081ab734a93920fa3cf13206d08b9
selamawit Getaneh <51340818+Selaml@users.noreply.github.com>
2025-08-09 09:44:03 +0300

    Revert "Ppii 1233 add intercepter and to encrpt and decrpt any request from backend"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,6 @@ configuration values obtained from a ConfigService. */
     AppConfigModule,
     CoreModule,
     SharedModule,
-    
     ThrottlerModule.forRoot({
       ttl: 60, // The time-to-live for each request count record (in seconds)
       limit: 10, // The maximum number of requests allowed in the given time period
