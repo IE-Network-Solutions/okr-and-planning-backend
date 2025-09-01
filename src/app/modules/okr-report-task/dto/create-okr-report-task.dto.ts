20d6e5ff5270ba884874a797dfe5f2702056fa6f
unknown <selamawitlb@gmail.com>
2025-09-01 11:06:52 +0300

    fix float

--- src/app/modules/okr-report-task/dto/create-okr-report-task.dto.ts
+++ src/app/modules/okr-report-task/dto/create-okr-report-task.dto.ts
@@ -1,6 +1,7 @@
-import { IsBoolean, IsOptional, IsString, IsUUID } from 'class-validator';
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsString, IsUUID } from 'class-validator';
 import { CreateFailureReasonDto } from '../../failure-reason/dto/create-failure-reason.dto';
 import { ReportStatusEnum } from '@root/src/core/interfaces/reportStatus.type';
+import { Transform } from 'class-transformer';
 
 // export class ReportTaskDTO {
 //   [key: string]: {
@@ -12,13 +13,37 @@ import { ReportStatusEnum } from '@root/src/core/interfaces/reportStatus.type';
 //     failureReason?: CreateFailureReasonDto;
 //   };
 
-export type ReportTaskInput = Record<
-  string,
-  {
-    status: ReportStatusEnum; // Required field
-    failureReasonId?: string; // Optional field
-    isAchieved?: boolean; // Optional field
-    actualValue?: number; // Optional field
-    customReason?: string; // Optional field
-  }
->;
+export class ReportTaskItem {
+  @IsEnum(ReportStatusEnum)
+  status: ReportStatusEnum;
+
+  @IsOptional()
+  @IsString()
+  failureReasonId?: string;
+
+  @IsOptional()
+  @IsBoolean()
+  isAchieved?: boolean;
+
+  @IsOptional()
+  @Transform(({ value }) => value ? parseFloat(value) : undefined)
+  @IsNumber({ maxDecimalPlaces: 3 })
+  actualValue?: number;
+
+  @IsOptional()
+  @IsString()
+  customReason?: string;
+}
+
+export type ReportTaskInput = Record<string, ReportTaskItem>;
+
+// export type ReportTaskInput = Record<
+//   string,
+//   {
+//     status: ReportStatusEnum; // Required field
+//     failureReasonId?: string; // Optional field
+//     isAchieved?: boolean; // Optional field
+//     actualValue?: number; // Optional field
+//     customReason?: string; // Optional field
+//   }
+// >;
